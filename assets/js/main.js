f81a2eadbf9683d899dd267738dcaa37cb6cfc24
AlexandreSteinmann <140978733+AlexandreSteinmann@users.noreply.github.com>
2023-09-05 13:09:54 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -168,10 +168,10 @@
     typed_strings = typed_strings.split(',')
     new Typed('.typed', {
       strings: typed_strings,
-      loop: true,
-      typeSpeed: 100,
-      backSpeed: 50,
-      backDelay: 2000
+      loop: false,
+      typeSpeed: 30,
+      backSpeed: 25,
+      backDelay: 1500
     });
   }
 
